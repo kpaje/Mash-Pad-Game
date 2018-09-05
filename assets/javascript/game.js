d99c706b29b7d08dd30609197db61f1c6c3c0474
admin <admin@its-st-cts0622d.kcls.org>
2018-09-05 16:03:42 -0700

    updated to proper sum/results and scoring

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,75 +1,68 @@
-let gameNumber = 0
-let wins = 0;
-let losses = 0;
-let score = 0;
+$(document).ready(function() {
+    let gameNumber = 0;
+    let wins = 0;
+    let losses = 0;
+    let score = 0;
 
 
-var game = {
-    randomNumber: function(min, max) {
-        return Math.floor(Math.random()*(max-min+1)+min);
-    },
+    var game = {
+        randomNumber: function(min, max) {
+            return Math.floor(Math.random()*(max-min+1)+min);
+        },
 
-    generateGameValue: function() {
-        gameNumber = this.randomNumber(19, 120);
-        $('.gameNumber').append(gameNumber);
-    },
+        generateGameValue: function() {
+            gameNumber = this.randomNumber(19, 120);
+            $('.gameNumber').text(gameNumber);
+        },
 
-    assignIntValues: function() {
-        $('.crystal').each(function() {
-            intValue = game.randomNumber(1, 19);
-            $(this).append('<button>' + intValue + '</button>')
-            $(this).attr({
-                'value': intValue
+        assignIntValues: function() {
+            $('.crystal').each(function() {
+                var intValue = game.randomNumber(1, 19);
+                $(this).html('<button>'+ intValue + '</button>')
+                $(this).val(intValue);
             });
-        });
-    },
-    
-    addToScore: function() {
-        intValue = parseInt($('.crystal').val());
-        score =  intValue + score;
-        console.log(score);
-        // $('.score').append(intValue);
-    },
-    
-    resetGame: function() {
-        score = 0;
-        this.randomNumber() = gameNumber;
-        this.generateIntValues();
-    },
+        },
+        
+        addToScore: function(userSelection) {
+            score += +$(userSelection).val();
+            $('.score').text(score);
+        },
+        
+        resetGame: function() {
+            score = 0;
+            this.randomNumber() = gameNumber;
+            this.generateIntValues();
+        },
 
-    loseGame: function() {
-        if (score > randomNumber) {
-            losses++;
-            this.resetGame()
-        };
-    },
-    
-    winGame: function() {
-        if (score == randomNumber) {
-            wins++;
-            this.resetGame();
-        };
-    },
-};
+        loseGame: function() {
+            if (score > gameNumber) {
+                $('.losses').text(losses++);
+                this.resetGame()
+            };
+        },
+        
+        winGame: function() {
+            if (score == gameNumber) {
+                $('.wins').text(wins++);
+                this.resetGame();
+            };
+        },
+    };
 
-var eventHandlers = {
-    addToScore: $('.crystal').click(function() {
-        // game.assignIntValues();
-        game.addToScore();
-    }),
+    var eventHandlers = {
+        addToScore: $('.crystal').click(function() {
+           game.addToScore(this);
+        }),
 
-    checkTotal: $('crystal').click(function() { 
-        loseGame();
-        winGame();
-    }),
-};
+        checkTotal: $('crystal').click(function() { 
+            game.loseGame();
+            game.winGame();
+        }),
 
+        intializeGame: $('.newGame').click(function() {
+            game.assignIntValues()
+            game.generateGameValue();
+        })
+    };
 
-game.generateGameValue();
-game.assignIntValues();
-
-console.log(
-    $('.crystal').each(function() {
-        console.log($('.crystal').val());
-    })
-);
+});
